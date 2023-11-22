9050a5dcc6893d7a3b6cf937ea127907d24a82eb
Pramath Naik <pramathbnaik@gmail.com>
2023-11-22 14:49:27 +0530

    api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 
 import streamlit
 import pandas
+import requests
 
 
 streamlit.title("Parents new healthy diner")
@@ -18,3 +19,5 @@ selected_fruits  =streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[selected_fruits]
 
 streamlit.dataframe(fruits_to_show)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
