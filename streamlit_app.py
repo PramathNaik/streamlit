ff082595d26dbd951dcdf40031d78aee9aef3f1f
Pramath Naik <pramathbnaik@gmail.com>
2023-11-22 10:21:37 +0530

    added df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 
 import streamlit
+import pandas
+
 
 streamlit.title("Parents new healthy diner")
 
@@ -10,3 +12,5 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
